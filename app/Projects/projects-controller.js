4a669075920fd268b5982e37fc6bc7423491d083
antodobrev <anton.dobrev@gmail.com>
2016-04-27 00:04:16 +0300

    added project page and some styles

--- app/Projects/projects-controller.js
+++ app/Projects/projects-controller.js
@@ -5,11 +5,28 @@ angular.module('IssueTruck.projects', ['ngRoute', 'IssueTruck.projects.getter'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/projects', {
             templateUrl: 'Projects/projects.html',
+            controller: 'ProjectsController'
+        });
+        $routeProvider.when('/project/:id', {
+            templateUrl: 'Projects/projectPage.html',
             controller: 'ProjectController'
         });
     }])
 
     .controller('ProjectController', [
+        '$scope',
+        'projectsGetter',
+        '$routeParams',
+        function ($scope, projectsGetter, $routeParams) {
+            var projectId = $routeParams.id;
+            projectsGetter.getProjectById(projectId).then(function (projectData) {
+                console.log(projectData.data);
+                $scope.project = projectData.data;
+            })
+        }
+    ])
+
+    .controller('ProjectsController', [
         '$scope',
         'projectsGetter',
         function($scope, projectsGetter) {
