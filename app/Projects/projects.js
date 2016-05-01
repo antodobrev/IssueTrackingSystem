21544cc7fdae9dee2548ca2ba2736616fc37ef34
antodobrev <anton.dobrev@gmail.com>
2016-05-02 01:03:45 +0300

    add project

--- app/Projects/projects.js
+++ app/Projects/projects.js
@@ -65,8 +65,56 @@ angular.module('IssueTruck.projects.getter', [])
                 return defered.promise;
             }
 
+            function getAllLabels(input) {
+                var defered = $q.defer();
+
+                $http.defaults.headers.common.Authorization = 'Bearer ' + sessionStorage.token;
+
+                $http.get(BASE_URL + 'labels/?filter=' + input).then(function (response) {
+                    defered.resolve(response);
+                }, function (err) {
+                    console.log(err);
+                });
+
+                return defered.promise;
+            }
+
             function addProject(projectData) {
+                var defered = $q.defer();
+
+                var config = {
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                };
+
+                $http.defaults.headers.common.Authorization = 'Bearer ' + sessionStorage.token;
+
+                var data = 'name='
+                    + projectData.Name
+                    + '&description='
+                    + projectData.Description
+                    + '&projectKey='
+                    + projectData.ProjectKey
+                    + '&leadId='
+                    + projectData.leadId;
+
+                var counter = 0;
+                for (var label in projectData.labels) {
+                    data += '&labels[' + counter + '].Name=' + projectData.labels[label];
+                    counter++;
+                }
+                var priorities = projectData.priorities.split( /,\s/);
+                priorities.forEach(function (p) {
+                    data += '&priorities[' + counter + '].Name=' + p;
+                });
                 
+                console.log(data);
+                $http.post(BASE_URL + 'projects', data, config).then(function (response) {
+                    defered.resolve(response);
+                }, function (err) {
+                    console.log(err);
+                });
+
+                return defered.promise;
             }
 
             return {
@@ -74,7 +122,8 @@ angular.module('IssueTruck.projects.getter', [])
                 getProjectById: getProjectById,
                 getProjectIssues: getProjectIssues,
                 getProjectsByLeaderId: getProjectsByLeaderId,
-                addProject: addProject
+                addProject: addProject,
+                getAllLabels: getAllLabels
             }
         }
     ]);
--- app/projects/add-project-controller.js
+++ app/projects/add-project-controller.js
@@ -17,17 +17,16 @@ angular.module('IssueTruck.projects.AddProjectController', ['ngRoute', 'IssueTru
 
             $scope.generateKey = function (name) {
                 var key = [];
-                name.split(/\s/).forEach(function(el) { 
+                name.split(/\s/).forEach(function (el) {
                     key.push(el[0])
                 });
-                $scope.projectData.ProjectKey = key.join('');
+                $scope.project.ProjectKey = key.join('');
             };
 
             $scope.loadingUsers = authentication.getAllUsers()
                 .then(function (response) {
                     $scope.users = response.data;
                     $scope.formatLead = function (model) {
-                        debugger;
                         for (var i = 0; i < $scope.users.length; i++) {
                             if (model === $scope.users[i].Id) {
                                 return $scope.users[i].Username;
@@ -38,20 +37,41 @@ angular.module('IssueTruck.projects.AddProjectController', ['ngRoute', 'IssueTru
                     console.log(error);
                 });
 
-           // authentication.getAllUsers().then(function (response) {
-           //     $scope.allUsers = response.data;
-           //     console.log($scope.allUsers);
-           // });
+            $scope.numberOfLabels = new Array(1);
+
+            $scope.increaseLabels = function () {
+                $scope.numberOfLabels.push("");
+            };
+
+            $scope.loadingLabels = function (input) {
+                projectsGetter.getAllLabels(input)
+                    .then(function (response) {
+                        $scope.labels = response.data;
+                        $scope.formatLabel = function (model) {
+                            for (var i = 0; i < $scope.labels.length; i++) {
+                                if (model === $scope.labels[i].Name) {
+                                    return $scope.labels[i].Name;
+                                }
+                            }
+                        };
+                    });
+            }
+
+            // authentication.getAllUsers().then(function (response) {
+            //     $scope.allUsers = response.data;
+            //     console.log($scope.allUsers);
+            // });
 
             $scope.addNewProject = function (projectData) {
                 var collectedProjectData = projectData;
-                console.log(collectedProjectData);
-                $scope.projectData.ProjectKey = generateProjectKey(projectData.Name);
-                console.log($scope.projectData.ProjectKey);
-                //projectsGetter.addProject(collectedProjectData).then(function (projectData) {
-              //      console.log(projectData.data);
-               //     $scope.project = projectData.data;
-               // })
+                console.log(collectedProjectData.labels);
+                //$scope.projectData.ProjectKey = generateProjectKey(projectData.Name);
+                //console.log($scope.projectData.ProjectKey);
+                projectsGetter.addProject(collectedProjectData)
+                    .then(function (projectData) {
+                        console.log(projectData.data);
+                        $scope.project = projectData.data;
+                    })
             }
 
         }
